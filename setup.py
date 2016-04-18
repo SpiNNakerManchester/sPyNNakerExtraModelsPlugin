6241149fdab75c5d454511356068e4edad30f24b
alan-stokes <alan.barry.stokes@gmail.com>
2016-04-18 10:25:16 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -6,15 +6,18 @@ setup(
     description="Extra models not in PyNN",
     url="https://github.com/SpiNNakerManchester/sPyNNakerExtraModelsPlugin",
     packages=['spynnaker_extra_pynn_models',
+              'spynnaker_extra_pynn_models',
               'spynnaker_extra_pynn_models.model_binaries',
-              'spynnaker_extra_pynn_models.neural_properties',
-              'spynnaker_extra_pynn_models.neural_properties.synapse_dynamics',
-              'spynnaker_extra_pynn_models.neural_properties.synapse_dynamics.dependences',
+              'spynnaker_extra_pynn_models.neuron',
               'spynnaker_extra_pynn_models.neuron.additional_inputs',
               'spynnaker_extra_pynn_models.neuron.builds',
               'spynnaker_extra_pynn_models.neuron.neural_models',
+              'spynnaker_extra_pynn_models.neuron.plasticity',
               'spynnaker_extra_pynn_models.neuron.synapse_types',
-              'spynnaker_extra_pynn_models.neuron.threshold_types'],
+              'spynnaker_extra_pynn_models.neuron.threshold_types',
+              'spynnaker_extra_pynn_models.neuron.plasticity.stdp',
+              'spynnaker_extra_pynn_models.neuron.plasticity.stdp.synapse_structure',
+              'spynnaker_extra_pynn_models.neuron.plasticity.stdp.timing_dependence'],
     package_data={'spynnaker_extra_pynn_models.model_binaries': ['*.aplx']},
-    install_requires=['SpyNNaker == 2015.005.01']
+    install_requires=['SpyNNaker == 2016.001']
 )
