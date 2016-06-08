02f2db34b68f9425bd907e5c3db4c2d3a0d04b3f
alan-stokes <alan.barry.stokes@gmail.com>
2016-06-08 17:10:00 +0100

    fixed additional input ca2 adaptive to meet new interface. Dont have test scripts to test.

--- spynnaker_extra_pynn_models/neuron/additional_inputs/additional_input_ca2_adaptive.py
+++ spynnaker_extra_pynn_models/neuron/additional_inputs/additional_input_ca2_adaptive.py
@@ -10,59 +10,69 @@ import numpy
 
 class AdditionalInputCa2Adaptive(AbstractAdditionalInput):
 
-    def __init__(self, n_neurons, machine_time_step, tau_ca2, i_ca2, i_alpha):
+    @staticmethod
+    def default_parameters():
+        return {'tau_ca2': 50.0, 'i_ca2': 0.0, 'i_alpha': 0.1}
+
+    @staticmethod
+    def fixed_parameters():
+        return {}
+
+    @staticmethod
+    def state_variables():
+        params = list()
+        return params
+
+    @staticmethod
+    def is_array_parameters():
+        return {}
+
+    def __init__(self, bag_of_neurons):
         AbstractAdditionalInput.__init__(self)
 
-        self._n_neurons = n_neurons
-        self._machine_time_step = machine_time_step
-
-        self._tau_ca2 = utility_calls.convert_param_to_numpy(
-            tau_ca2, n_neurons)
-        self._i_ca2 = utility_calls.convert_param_to_numpy(
-            i_ca2, n_neurons)
-        self._i_alpha = utility_calls.convert_param_to_numpy(
-            i_alpha, n_neurons)
+        self._n_neurons = len(bag_of_neurons)
+        self._atoms = bag_of_neurons
 
     @property
     def tau_ca2(self):
-        return self._tau_ca2
+        return self._get_param('tau_ca2', self._atoms)
 
     @tau_ca2.setter
     def tau_ca2(self, tau_ca2):
-        self._tau_ca2 = utility_calls.convert_param_to_numpy(
-            tau_ca2, self._n_neurons)
+        self._set_param('tau_ca2', tau_ca2, self._atoms)
 
     @property
     def i_ca2(self):
-        return self._i_ca2
+        return self._get_param('i_ca2', self._atoms)
 
     @i_ca2.setter
     def i_ca2(self, i_ca2):
-        self._i_ca2 = utility_calls.convert_param_to_numpy(
-            i_ca2, self._n_neurons)
+        self._set_param('i_ca2', i_ca2, self._atoms)
 
     @property
     def i_alpha(self):
-        return self._i_alpha
+        return self._get_param('i_alpha', self._atoms)
 
     @i_alpha.setter
     def i_alpha(self, i_alpha):
-        self._i_alpha = utility_calls.convert_param_to_numpy(
-            i_alpha, self._n_neurons)
+        self._set_param('i_alpha', i_alpha, self._atoms)
 
-    @property
-    def _exp_tau_ca2(self):
-        return numpy.exp(float(-self._machine_time_step) /
-                         (1000.0 * self._tau_ca2))
+    def _exp_tau_ca2(self, atom_id):
+        return numpy.exp(float(
+            -self._atoms[atom_id].population_parameters["machine_time_step"]) /
+            (1000.0 * self._atoms[atom_id].get('tau_ca2')))
 
     def get_n_parameters(self):
         return 3
 
-    def get_parameters(self):
+    def get_parameters(self, atom_id):
         return [
-            NeuronParameter(self._exp_tau_ca2, DataType.S1615),
-            NeuronParameter(self._i_ca2, DataType.S1615),
-            NeuronParameter(self._i_alpha, DataType.S1615)
+            NeuronParameter(
+                self._exp_tau_ca2(atom_id), DataType.S1615),
+            NeuronParameter(
+                self._atoms[atom_id].get('i_ca2'), DataType.S1615),
+            NeuronParameter(
+                self._atoms[atom_id].get('i_alpha'), DataType.S1615)
         ]
 
     def get_n_cpu_cycles_per_neuron(self):
