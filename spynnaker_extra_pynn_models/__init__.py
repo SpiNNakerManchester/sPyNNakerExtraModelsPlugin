f33dbceb1aad8acc1f48d90f6c59d5bfff9386e7
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2017-02-17 13:20:30 +0000

    Flake8

--- spynnaker_extra_pynn_models/__init__.py
+++ spynnaker_extra_pynn_models/__init__.py
@@ -13,14 +13,18 @@ from spynnaker_extra_pynn_models.neuron.plasticity.stdp.timing_dependence\
     .timing_dependence_vogels_2011\
     import TimingDependenceVogels2011 as Vogels2011Rule
 
+__all__ = ['IF_curr_delta', 'IF_curr_exp_ca2_adaptive', 'IF_cond_exp_stoc',
+           'RecurrentRule', 'Vogels2011Rule', 'model_binaries']
+
 
 def _init_module():
-    import logging
+    # import logging
     import os
     import spynnaker.pyNN
 
     # Register this path with SpyNNaker
     spynnaker.pyNN.register_binary_search_path(os.path.dirname(
         model_binaries.__file__))
+
 
 _init_module()
