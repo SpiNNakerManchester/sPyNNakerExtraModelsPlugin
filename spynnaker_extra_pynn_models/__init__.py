0d75e02a0d5d594b2515070e8df94c8ecf818657
Christian <brenninc@cs.man.ac.uk>
2017-02-20 16:50:59 +0000

    moved imports to not run in readthedocs

--- spynnaker_extra_pynn_models/__init__.py
+++ spynnaker_extra_pynn_models/__init__.py
@@ -1,20 +1,7 @@
-from spynnaker_extra_pynn_models.neuron.builds.if_curr_delta \
-    import IFCurrDelta as IF_curr_delta
-from spynnaker_extra_pynn_models.neuron.builds.if_curr_exp_ca2_adaptive \
-    import IFCurrExpCa2Adaptive as IF_curr_exp_ca2_adaptive
-from spynnaker_extra_pynn_models.neuron.builds.if_cond_exp_stoc \
-    import IFCondExpStoc as IF_cond_exp_stoc
-from spynnaker_extra_pynn_models.neuron.plasticity.stdp.timing_dependence\
-    .timing_dependence_recurrent\
-    import TimingDependenceRecurrent as RecurrentRule
-from spynnaker_extra_pynn_models.neuron.plasticity.stdp.timing_dependence\
-    .timing_dependence_vogels_2011\
-    import TimingDependenceVogels2011 as Vogels2011Rule
 import os
 
 __all__ = ['IF_curr_delta', 'IF_curr_exp_ca2_adaptive', 'IF_cond_exp_stoc',
            'RecurrentRule', 'Vogels2011Rule', 'model_binaries']
-
 
 def _init_module():
     # import logging
@@ -27,4 +14,17 @@ def _init_module():
 
 
 if os.environ.get('READTHEDOCS', None) != 'True':
+    from spynnaker_extra_pynn_models.neuron.builds.if_curr_delta import \
+        IFCurrDelta as IF_curr_delta
+    from spynnaker_extra_pynn_models.neuron.builds.if_curr_exp_ca2_adaptive \
+        import IFCurrExpCa2Adaptive as IF_curr_exp_ca2_adaptive
+    from spynnaker_extra_pynn_models.neuron.builds.if_cond_exp_stoc import \
+        IFCondExpStoc as IF_cond_exp_stoc
+    from spynnaker_extra_pynn_models.neuron.plasticity.stdp.timing_dependence \
+        .timing_dependence_recurrent \
+        import TimingDependenceRecurrent as RecurrentRule
+    from spynnaker_extra_pynn_models.neuron.plasticity.stdp.timing_dependence \
+        .timing_dependence_vogels_2011 \
+        import TimingDependenceVogels2011 as Vogels2011Rule
+
     _init_module()
