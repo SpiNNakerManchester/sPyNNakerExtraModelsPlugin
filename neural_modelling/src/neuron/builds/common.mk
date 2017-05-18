cb01679f9fa0f37af35727e7079f186db6b74679
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2017-05-18 15:51:42 +0100

    Update the internal structure.

---
+++ neural_modelling/src/neuron/builds/common.mk
@@ -0,0 +1,4 @@
+include ../../../paths.mk
+include $(NEURAL_MODELLING_DIRS)/src/paths.mk
+include $(NEURAL_MODELLING_DIRS)/src/neuron/builds/classes.mk
+include $(NEURAL_MODELLING_DIRS)/src/neuron/builds/neural_build.mk
