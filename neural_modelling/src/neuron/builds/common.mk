7592c47b8ea11474872b2f42442172f8f926cd52
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2017-05-23 13:25:04 +0100

    This makes the builds only conventionally broken

--- neural_modelling/src/neuron/builds/common.mk
+++ neural_modelling/src/neuron/builds/common.mk
@@ -1,5 +1,5 @@
-STDP := $(TIMING_DEPENDENCE) $(WEIGHT_DEPENDENCE)
 include ../../../paths.mk
 include $(NEURAL_MODELLING_DIRS)/src/paths.mk
+STDP := $(TIMING_DEPENDENCE) $(WEIGHT_DEPENDENCE)
 include $(NEURAL_MODELLING_DIRS)/src/neuron/builds/classes.mk
 include $(NEURAL_MODELLING_DIRS)/src/neuron/builds/neural_build.mk
