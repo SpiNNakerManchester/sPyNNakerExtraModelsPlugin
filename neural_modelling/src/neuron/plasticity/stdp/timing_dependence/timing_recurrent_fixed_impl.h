3644c6e92e0bf51550668bd51afa9116f00f5eb0
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2017-06-13 11:44:15 +0100

    Fixed line length issue

--- neural_modelling/src/neuron/plasticity/stdp/timing_dependence/timing_recurrent_fixed_impl.h
+++ neural_modelling/src/neuron/plasticity/stdp/timing_dependence/timing_recurrent_fixed_impl.h
@@ -14,30 +14,31 @@ typedef struct {
     uint32_t post_window_length;
 } plasticity_trace_region_data_t;
 
-#include "neuron/plasticity/stdp/synapse_structure/synapse_structure_weight_state_accumulator_impl.h"
+#include "neuron/plasticity/stdp/synapse_structure/\
+synapse_structure_weight_state_accumulator_impl.h"
 #include "timing_recurrent_common.h"
 
 static inline bool timing_recurrent_in_pre_window(
-	uint32_t time_since_last_event,
-	update_state_t previous_state) {
+    uint32_t time_since_last_event,
+    update_state_t previous_state) {
     return (time_since_last_event
-	    < plasticity_trace_region_data.pre_window_length);
+        < plasticity_trace_region_data.pre_window_length);
 }
 
 static inline bool timing_recurrent_in_post_window(
-	uint32_t time_since_last_event,
-	update_state_t previous_state) {
+    uint32_t time_since_last_event,
+    update_state_t previous_state) {
     return (time_since_last_event
-	    < plasticity_trace_region_data.post_window_length);
+        < plasticity_trace_region_data.post_window_length);
 }
 
 static inline update_state_t timing_recurrent_calculate_pre_window(
-	update_state_t previous_state) {
+    update_state_t previous_state) {
     return previous_state;
 }
 
 static inline update_state_t timing_recurrent_calculate_post_window(
-	update_state_t previous_state) {
+    update_state_t previous_state) {
     return previous_state;
 }
 
