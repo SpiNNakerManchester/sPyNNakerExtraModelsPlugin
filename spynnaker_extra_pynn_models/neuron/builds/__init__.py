71fa4c60042d1f4a3d01dbf417aec5a6a3dd4ff7
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2017-06-27 12:05:03 +0100

    Missed a spot

---
+++ spynnaker_extra_pynn_models/neuron/builds/__init__.py
@@ -0,0 +1,5 @@
+from .if_cond_exp_stoc import IFCondExpStoc
+from .if_curr_delta import IFCurrDelta
+from .if_curr_exp_ca2_adaptive import IFCurrExpCa2Adaptive
+
+__all__ = ["IFCondExpStoc", "IFCurrDelta", "IFCurrExpCa2Adaptive"]
