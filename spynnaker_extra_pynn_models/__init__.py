ede22a82099eb8e2eef9365ef6b595c0916130f3
oliverrhodes <mbassor2@cspc457.cs.man.ac.uk>
2017-07-13 18:36:37 +0100

    created extra_models version of additional synapses

---
+++ spynnaker_extra_pynn_models/__init__.py
@@ -0,0 +1,9 @@
+from spynnaker.pyNN.abstract_spinnaker_common import AbstractSpiNNakerCommon
+from spynnaker_extra_pynn_models import model_binaries
+
+import os
+
+# This adds the model binaries path to the paths searched by sPyNNaker
+# Seems quite an ugly way of doing it, though... ?
+executable_finder = AbstractSpiNNakerCommon._EXECUTABLE_FINDER
+executable_finder.add_path(os.path.dirname(model_binaries.__file__))
